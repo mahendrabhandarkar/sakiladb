2c27d2ac65d1973f158c537bbc05e42c13f57437
Mahendra Bhandarkar <mahendra.bhandarkar@expleogroup.com>
2023-02-23 17:43:14 +0530

    Added New Entry

---
+++ src/test/java/com/sakila/sakilaproject/SakilaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sakila.sakilaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakilaprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
